0fad565136113c2a15ba0427290018c83970a6da
Tarmo Aidantausta <tarmo.aidantausta@futurice.com>
2017-04-18 14:19:47 +0300

    Update dependencies
    - reagent 0.6.1
    - org.clojure/clojurescript 1.9.293
    - cljsjs/pikaday 1.5.1-2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cljs-pikaday "0.1.3"
+(defproject cljs-pikaday "0.1.4"
   :description "ClojureScript interface to the Pikaday JS date picker"
   :url "https://github.com/timgilbert/cljs-pikaday"
   :license {:name "MIT"
@@ -7,7 +7,7 @@
   :source-paths ["src"]
 
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [reagent "0.6.0-rc"]
-                 [org.clojure/clojurescript "1.9.93" :scope "provided"]
-                 [cljsjs/pikaday "1.4.0-1"]
+                 [reagent "0.6.1"]
+                 [org.clojure/clojurescript "1.9.293" :scope "provided"]
+                 [cljsjs/pikaday "1.5.1-2"]
                  [camel-snake-kebab "0.4.0" :exclusions [org.clojure/clojure]]])
